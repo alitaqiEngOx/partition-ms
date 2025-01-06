23dd3c08bebf1cf61701ee24cafb13b5ae3812bb
Ali Taqi <alitaqi94.developer@gmail.com>
2025-01-06 12:12:01 +0000

    loggers activated & code used

--- src/partition_ms/pipeline.py
+++ src/partition_ms/pipeline.py
@@ -10,49 +10,42 @@ def break_by_snapshots(
 ) -> None:
     """
     """
-    # main table
-    print("MAIN.....")
     msin = table(f"{msin_dir}", ack=False)
     msin.copy(f"{msout_dir}", deep=True, copynorows=True)
-
-    msout = table(f"{msout_dir}", ack=False, readonly=False)
-    msout.addrows(filter[1]-filter[0])
-
-    for colname in msin.colnames():
-        try:
-            coldata = msin.getcol(colname)
-
-        except Exception as e:
-            print(f"skipping={colname}: {e}")
-            continue
-
-        msout.putcol(colname, coldata[filter[0]:filter[1]])
-        print(f"copied={colname}")
-
-    msin.close()
-    msout.close()
-
-    # all other tables
-    tblnames = [
+    tblnames = ["MAIN"] + [
         tbl.name for tbl in msout_dir.iterdir() if tbl.is_dir()
     ]
 
     for tblname in tblnames:
-        print(f"\n{tblname}.....")
-        msin = table(f"{msin_dir.joinpath(tblname)}", ack=False)
-        msout = table(f"{msout_dir.joinpath(tblname)}", ack=False, readonly=False)
-        msout.addrows(msin.nrows())
+        logger.info(f"Copying {tblname} table...")
+
+        if tblname == "MAIN":
+            logger.info(f"Only rows {filter[0]}-{filter[1]} out of {msin.nrows()}\n  |")
+
+            msout = table(f"{msout_dir}", ack=False, readonly=False)
+            msout.addrows(filter[1]-filter[0])
+
+        else:
+            msin = table(f"{msin_dir.joinpath(tblname)}", ack=False)
+            msout = table(f"{msout_dir.joinpath(tblname)}", ack=False, readonly=False)
+            msout.addrows(msin.nrows())
 
         for colname in msin.colnames():
             try:
                 coldata = msin.getcol(colname)
-
             except Exception as e:
-                print(f"skipping={colname}: {e}")
+                logger.warning(f"Skipping {colname} column")
                 continue
 
-            msout.putcol(colname, coldata)
-            print(f"copied={colname}")
+            if tblname == "MAIN":
+                msout.putcol(colname, coldata[filter[0]:filter[1]])
+            else:
+                msout.putcol(colname, coldata)
+
+            if colname == msin.colnames()[-1]:
+                logger.info(f"Copied {colname} column\n  |")
+            else:
+                logger.info(f"Copied {colname} column")
 
         msin.close()
         msout.close()
